04e4a874266ec1a01db0cb6bd1efdd4c23546993
John Roller <jtroller33@gmail.com>
2023-02-24 09:45:42 -0600

    .

---
+++ intermediate_syntax/py crash course/lists.py
@@ -0,0 +1,14 @@
+# lists are mutable in python
+
+# ---  list methods:
+
+def list_methods():
+    fruits = ["Pineapple", "Apple", "Banana", "Orange", "Mango"]
+    fruits.append("Kiwi")
+    print(fruits)
+    fruits.insert(0, "orange")
+    print(fruits)
+    
+    # normally you'd either add to the beginning with insert(0) or the end with append()
+    
+    
