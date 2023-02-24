96ea69c379c1990b42f148299b48bb737a0acefe
John Roller <jtroller33@gmail.com>
2023-02-24 09:57:40 -0600

    list methods

--- intermediate_syntax/py crash course/lists.py
+++ intermediate_syntax/py crash course/lists.py
@@ -1,14 +1,29 @@
 # lists are mutable in python
 
-# ---  list methods:
+# ---  CRUD list methods:
 
 def list_methods():
+    # define a list
     fruits = ["Pineapple", "Apple", "Banana", "Orange", "Mango"]
-    fruits.append("Kiwi")
-    print(fruits)
-    fruits.insert(0, "orange")
     print(fruits)
     
+    # add to the end of the list
+    fruits.append("Kiwi")
+    print(fruits)
+    # add to the beginning of the list
+    fruits.insert(0, "orange")
+    print(fruits)
     # normally you'd either add to the beginning with insert(0) or the end with append()
+    
+    # remove the first occurance from the list
+    fruits.remove("orange")
+    print(fruits)
+    
+    fruits.pop(2) # removes the item at the index
+    print(fruits)
+    
+    # replace an item in the list by its index
+    fruits[2] = "Strawberry"
+    print(fruits)
     
     
