f3b20b37a4e23efc32a129abfa9a29bb4c82c74a
John Roller <jtroller33@gmail.com>
2023-03-11 12:06:41 -0600

    .

--- intermediate_syntax/py crash course/dictionaries.py
+++ intermediate_syntax/py crash course/dictionaries.py
@@ -1,6 +1,14 @@
 # data in dictionaries is in key-value pairs
+# keys can't be duplicated
 
-first_dict = {'one': 1, 'two': 2, 'three': 3}
+def first_dict():
+    dict = {'one': 1, 'two': 2, 'three': 3}
+    # print("four" in dict) # False
+
+    # add to the end of a dictionary
+    dict["five"] = 5
+    print(dict)
 
 
+first_dict()
 
