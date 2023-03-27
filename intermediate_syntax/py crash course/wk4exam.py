6b2c1e0e8b563deab8081ffe3ba1f54877dd620d
John Roller <jtroller33@gmail.com>
2023-03-27 18:38:23 -0500

    exam dome

--- intermediate_syntax/py crash course/wk4exam.py
+++ intermediate_syntax/py crash course/wk4exam.py
@@ -16,6 +16,8 @@ def format_address():
     print(f"House number {house_number} on a street named {street_name.strip()}")
     
 
+
+
 def string_words(string):
     # Complete the return statement using both a string operation and 
     # a string method in a single line.
@@ -40,6 +42,37 @@ Aniyahs_list = ["Jacomo", "Emma", "Uli", "Nia", "Imani"]
 Imanis_list = ["Loik", "Gabriel", "Ahmed", "Soraya"]
 
 
-print(alphabetize_lists(Aniyahs_list, Imanis_list))
+# print(alphabetize_lists(Aniyahs_list, Imanis_list))
 # Should print: ['Ahmed', 'Emma', 'Gabriel', 'Imani', 'Jacomo', 'Loik', 'Nia', 'Soraya', 'Uli']
 
+
+
+def squares(start, end):
+    return [n*n for n in range(start, end+1)] 
+
+
+# print(squares(2, 3)) # Should print [4, 9]
+# print(squares(1, 5)) # Should print [1, 4, 9, 16, 25]
+# print(squares(0, 10)) # Should print [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+
+def countries(countries_dict):
+    result = ""
+    for key,value in countries_dict.items():
+        result += countries_dict[key]
+        print(result)
+
+
+# print(countries({"Africa": ["Kenya", "Egypt", "Nigeria"], "Asia":["China", "India", "Thailand"], "South America": ["Ecuador", "Bolivia", "Brazil"]}))
+
+
+
+    # countries = {
+    #     "Africa": ["Kenya", "Egypt", "Nigeria"], 
+    #     "Asia": ["China", "India", "Thailand"], 
+    #     "South America": ["Ecuador", "Bolivia", "Brazil"]
+    #     }
+
+
+host_addresses = {"router": "192.168.1.1", "localhost": "127.0.0.1", "google": "8.8.8.8"}
+print(host_addresses.keys())
