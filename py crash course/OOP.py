8ca54172faaf6b127da0f1e3458c82759792acd6
John Roller <jtroller33@gmail.com>
2023-03-29 14:26:40 -0500

    quiz done

--- py crash course/OOP.py
+++ py crash course/OOP.py
@@ -33,6 +33,8 @@ pun = "Dwight killed Sprinkles, and buried him too."
 print(f"Roses are {rose.color},\nViolets are {violet.color}.\n{pun}\n")
 
 
+# -------------------------------------------------------------------------------------------
+
 class Person:
     apples = 0
     ideas = 0
@@ -46,10 +48,8 @@ martin.apples = 2
 martin.ideas = 1
 
 def exchange_apples(you, me):
-
-#We're going to have Martin and Johanna exchange ALL their apples with one another.
-
-    you, me = me, you
+# We're going to have Martin and Johanna exchange ALL their apples with #one another.
+    you.apples, me.apples = me.apples, you.apples
     return you.apples, me.apples
     
 def exchange_ideas(you, me):
@@ -66,3 +66,87 @@ print("Johanna has {} ideas and Martin has {} ideas".format(johanna.ideas, marti
 
 
 
+
+# -------------------------------------------------------------------------------------------
+
+
+class City:
+	name = ""
+	country = ""
+	elevation = 0 
+	population = 0
+
+city1 = City()
+city1.name = "Cusco"
+city1.country = "Peru"
+city1.elevation = 3399
+city1.population = 358052
+
+city2 = City()
+city2.name = "Sofia"
+city2.country = "Bulgaria"
+city2.elevation = 2290
+city2.population = 1241675
+
+city3 = City()
+city3.name = "Seoul"
+city3.country = "South Korea"
+city3.elevation = 38
+city3.population = 9733509
+
+def max_elevation_city(min_population):
+	# Initialize the variable that will hold 
+# the information of the city with 
+# the highest elevation
+
+	highest_city = city1.elevation
+	return_city = City()
+
+	# Evaluate the 1st instance to meet the requirements:
+	# does city #1 have at least min_population and
+	# is its elevation the highest evaluated so far?
+	if city1.population >= min_population and city1.elevation >= highest_city:
+		return_city = city1
+	# Evaluate the 2nd instance to meet the requirements:
+	# does city #2 have at least min_population and
+	# is its elevation the highest evaluated so far?
+	if city2.population >= min_population and city2.elevation >= highest_city:
+		return_city = city2
+	# Evaluate the 3rd instance to meet the requirements:
+	# does city #3 have at least min_population and
+	# is its elevation the highest evaluated so far?
+	if city3.population >= min_population and city3.elevation >= highest_city:
+		return_city = city3
+
+	#Format the return string
+	if return_city.name:
+		return f"{return_city.name}, {return_city.country}"
+	else:
+		return ""
+
+print(max_elevation_city(100000)) # Should print "Cusco, Peru"
+print(max_elevation_city(1000000)) # Should print "Sofia, Bulgaria"
+print(max_elevation_city(10000000)) # Should print ""
+
+
+# -------------------------------------------------------------------------------------
+
+class Furniture:
+	color = ""
+	material = ""
+
+table = Furniture()
+table.color = "brown"
+table.material = "wood"
+
+couch = Furniture()
+couch.color = "red"
+couch.material = "leather"
+
+def describe_furniture(piece):
+	return f"This piece of furniture is made of {piece.color} {piece.material}"
+
+print(describe_furniture(table)) 
+# Should be "This piece of furniture is made of brown wood"
+print(describe_furniture(couch)) 
+# Should be "This piece of furniture is made of red leather"
