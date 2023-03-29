d1d98b301b114dc6c8d79d8b4f04c5e84298fee8
John Roller <jtroller33@gmail.com>
2023-03-29 14:01:17 -0500

    question 2

--- py crash course/OOP.py
+++ py crash course/OOP.py
@@ -32,3 +32,37 @@ pun = "Dwight killed Sprinkles, and buried him too."
 
 print(f"Roses are {rose.color},\nViolets are {violet.color}.\n{pun}\n")
 
+
+class Person:
+    apples = 0
+    ideas = 0
+
+johanna = Person()
+johanna.apples = 1
+johanna.ideas = 1
+
+martin = Person()
+martin.apples = 2
+martin.ideas = 1
+
+def exchange_apples(you, me):
+
+#We're going to have Martin and Johanna exchange ALL their apples with one another.
+
+    you, me = me, you
+    return you.apples, me.apples
+    
+def exchange_ideas(you, me):
+    #"you" and "me" will share our ideas with one another.
+
+    you.ideas += me.ideas
+    me.ideas = you.ideas
+    return you.ideas, me.ideas
+
+exchange_apples(johanna, martin)
+print("Johanna has {} apples and Martin has {} apples".format(johanna.apples, martin.apples))
+exchange_ideas(johanna, martin)
+print("Johanna has {} ideas and Martin has {} ideas".format(johanna.ideas, martin.ideas))
+
+
+
