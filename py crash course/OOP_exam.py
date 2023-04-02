2f05a856d583f685242e5329acf418bf49ce641a
John Roller <jtroller33@gmail.com>
2023-04-02 16:01:39 -0500

    OOP exam almost done

---
+++ py crash course/OOP_exam.py
@@ -0,0 +1,70 @@
+#Begin Portion 1#
+import random
+
+class Server:
+    def __init__(self):
+        """Creates a new server instance, with no active connections."""
+        self.connections = {}
+
+    def add_connection(self, connection_id):
+        """Adds a new connection to this server."""
+        connection_load = random.random()*10+1
+        self.connections[connection_id] = connection_load
+
+    def close_connection(self, connection_id):
+        """Closes a connection on this server."""
+        del self.connections[connection_id]
+
+    def load(self):
+        """Calculates the current load for all connections."""
+        total = 0
+        for load in self.connections.values():
+            total += load
+        return total
+
+    def __str__(self):
+        """Returns a string with the current load of the server"""
+        return "{:.2f}%".format(self.load())
+    
+
+
+
+#Begin Portion 2#
+class LoadBalancing:
+    def __init__(self):
+        """Initialize the load balancing system with one server"""
+        self.connections = {}
+        self.servers = [Server()]
+
+    def add_connection(self, connection_id):
+        """Randomly selects a server and adds a connection to it."""
+        server = random.choice(self.servers)
+        self.connections[connection_id] = server
+        self.servers.append(server)
+
+        # Add the connection to the dictionary with the selected server
+        # Add the connection to the server
+
+    def close_connection(self, connection_id):
+        """Closes the connection on the the server corresponding to connection_id."""
+        # Find out the right server
+        # Close the connection on the server
+        # Remove the connection from the load balancer
+
+    def avg_load(self):
+        """Calculates the average load of all servers"""
+        # Sum the load of each server and divide by the amount of servers
+        sum = 0
+        for load in self.connections.values():
+            sum += load
+        return sum
+
+    def ensure_availability(self):
+        """If the average load is higher than 50, spin up a new server"""
+        pass
+
+    def __str__(self):
+        """Returns a string with the load for each server."""
+        loads = [str(server) for server in self.servers]
+        return "[{}]".format(",".join(loads))
+#End Portion 2#
