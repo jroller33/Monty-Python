63ecdd6da93353c961266081ccda1201817911b5
John Roller <jtroller33@gmail.com>
2023-04-02 14:29:41 -0500

    modules

--- py crash course/modules.py
+++ py crash course/modules.py
@@ -1,2 +1,20 @@
 # modules are used to organize functions, classes etc together in a structured way
 
+# Python Standard Library is a group of modules included with Python
+
+# random module is good for making random numbers or random choices
+
+# https://pypi.org/
+
+import random
+
+print(random.randint(1, 10))
+
+import datetime
+now = datetime.datetime.now()
+# datetime module provides datetime class, which provides 'now()' method
+
+print(now)
+print(now.year)
+
+print(now + datetime.timedelta(days=28))
