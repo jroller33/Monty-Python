4fec1bceffcc09159d234ea14ed4d0e7c455b217
John Roller <jtroller33@gmail.com>
2023-05-03 17:08:30 -0500

    generate_report()

--- final_project.py
+++ final_project.py
@@ -19,14 +19,24 @@ def get_event_date(event):
 
 def current_users(events):
     events.sort(key=get_event_date)
+
     machines = {} 
 
     for event in events:
         if event.machine not in machines:
             machines[event.machine] = set()
+
         if event.type == "login":
             machines[event.machine].add(event.user)
+
         elif event.type == "logout":
             machines[event.machine].remove(event.user)
 
     return machines
+
+
+def generate_report(machines):
+    for machine, users in machines.items():
+        if len(users) > 0:
+            user_list = ", ".join(users)
+            print(f"{machine}: {user_list}")
